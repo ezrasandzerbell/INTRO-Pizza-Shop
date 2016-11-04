133efbd117f1fb29216ec0116e8794e0917a981d
Ezra Sandzer-Bell <Ezra@Ezras-MacBook-Pro.local>
2016-11-04 14:37:25 -0700

    create a fully functional template for pizza ordering. Still requires css and an 'add pizza' function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 var customerAddress = []
+var pizzaCost = [];
+var pizzaToppings = [];
+var pizzaToppingsNumber = [];
+var globalPizzaSizeCost = []
+var globalPizzaToppingsCost = []
+var globalTotalPizzaCost = []
 
 function Pizza(size, toppings) {
   this.pizzaSize = size;
@@ -9,6 +15,24 @@ Pizza.prototype.pizzaSizeSentence = function() {
   return "You've ordered a " + this.pizzaSize + " pizza with the following toppings:";
 }
 
+Pizza.prototype.pizzaSizeCost = function() {
+
+  if (this.pizzaSize === "Small") {
+    pizzaSizeCost = 8
+  }else if (this.pizzaSize === "Medium") {
+    pizzaSizeCost = 10
+  } else if (this.pizzaSize === "Large") {
+    pizzaSizeCost = 12
+  };
+    return pizzaSizeCost
+};
+
+Pizza.prototype.pizzaToppingsCost = function() {
+
+  pizzaMath = (pizzaToppingsNumber * .5);
+  return pizzaMath
+};
+
 // user interface logic
 $(document).ready(function() {
 
@@ -16,7 +40,6 @@ $(document).ready(function() {
     event.preventDefault();
 
     var pizzaDelivery = $("select.yesNoDelivery").val();
-    console.log(pizzaDelivery);
 
     if (pizzaDelivery === "delivery") {
       $("#deliveryDiv").hide();
@@ -30,31 +53,35 @@ $(document).ready(function() {
 
   $("form#enterAddress").submit(function(event) {
     event.preventDefault();
-      var firstName = $("input.new-last-name").val();
-      var lastName = $("input.new-first-name").val();
+      var firstName = $("input.new-first-name").val();
+      var lastName = $("input.new-last-name").val();
       var inputtedStreet = $("input.new-street").val();
       var inputtedCity = $("input.new-city").val();
       var inputtedState = $("input.new-state").val();
       customerAddress.push(firstName, lastName, inputtedStreet, inputtedCity, inputtedState);
-      console.log(customerAddress);
       $("#addressEntry").hide();
       $("#mainPage").show();
-
+      $("#deliveryDivFinal").show();
   });
+
+//Pizza Ordering Form
 
   $("form#orderPizza").submit(function(event) {
     event.preventDefault();
 
     var pizzaSize = $("select.pizzaSize").val();
-    var pizzaToppings = []
+
 
     $("input:checkbox[name=toppings]:checked").each(function(){
       var oneTopping = $(this).val();
       pizzaToppings.push(oneTopping);
     });
 
-    var pizzaOutput = new Pizza(pizzaSize, pizzaToppings);
+    var toppingsNumber = pizzaToppings.length;
+    pizzaToppingsNumber.push(toppingsNumber);
 
+
+    var pizzaOutput = new Pizza(pizzaSize, pizzaToppings);
 
     $("span#firstPizza").text(pizzaOutput.pizzaSizeSentence());
     $("ul#firstPizzaToppings").text("");
@@ -64,6 +91,18 @@ $(document).ready(function() {
     };
 
 
+    globalPizzaSizeCost.push(pizzaOutput.pizzaSizeCost());
+
+    globalPizzaToppingsCost.push(pizzaOutput.pizzaToppingsCost());
+
+    console.log(globalPizzaToppingsCost);
+
+    var theTotalPizzaCost = parseInt((globalPizzaSizeCost).toString()) + parseInt((globalPizzaToppingsCost).toString());
+
+    $("span#totalCost").text(theTotalPizzaCost);
+    for (i = 0; i <= (customerAddress.length-1); i++) {
+      $("ul#deliveryAddress").append(customerAddress[i] + "<br>")
+    };
     $("#customerOrderResult").show();
 
   });
